45657757cd0d6b1cbf1b55c24ccb4664db374f23
AmrElsherbiny <64423818+AmrElsherbiny@users.noreply.github.com>
2022-05-22 00:34:04 +0200

    Update reciever.py
    Receiving files without giving out of order packets messages

--- reciever.py
+++ reciever.py
@@ -1,39 +1,26 @@
 from socket import *
-# from BadNet5 import *
-import pickle
-import hashlib
+from threading import Thread
 import sys
-import os
-import math
-import time
 
-
-
-ip = "192.168.1.8"
-PORT = 2022
-
-sock = socket(AF_INET,SOCK_DGRAM)
-sock.bind((ip, PORT))
-
-
-seqnum = 1
-MSS = 100
-
-
-f = open("message.png", "wb")
-
-
-
-print('Recieving')
-while True:
-    data, addr = sock.recvfrom(MSS) 
-    #data = pickle.loads(data)
-    print("received message: %s" % data)
-    f.write(data)
-
-    #if (data.decode()=='END'):# if file ended
-     # break
-
-f.close()
-print('closed connection')
-#print(f'File size is : {os.path.getsize(f)}')
+clientsocket = socket(AF_INET, SOCK_DGRAM)
+print("Started Client UDP File Transfer!");
+for i in range(3):
+    clientsocket.sendto(sys.argv[1].encode(),(sys.argv[2],int(sys.argv[3])))
+    print("Waiting for responce from the server (Trial #{})...".format(i+1))
+    received, address = clientsocket.recvfrom(1024)
+    if received:
+        curr_packet = -1
+        file = open(sys.argv[1], "wb")
+        while True:
+            if received[:2] == (curr_packet+1).to_bytes(2, 'big'):
+                file.write(received[2:-1])
+                curr_packet += 1
+                print("Received packet in order ID",curr_packet) 
+            if curr_packet>-1: clientsocket.sendto((curr_packet).to_bytes(2,'big'),address)
+            if received[-1:] == b'\xff':
+                print("Client UDP File Transfer has successfully finished!")
+                break
+            received, address = clientsocket.recvfrom(1024)
+        file.close()
+        break
+else: print("Server connection failed after 3 trials...")
