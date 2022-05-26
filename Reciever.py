b582aa65492142783f7aae6a80a0506d0c97f651
AmrElsherbiny <64423818+AmrElsherbiny@users.noreply.github.com>
2022-05-26 07:54:38 +0200

    Update Reciever.py
    Fixed some issues in code

--- Reciever.py
+++ Reciever.py
@@ -3,7 +3,6 @@ from threading import Thread
 from time import time
 from time import ctime
 from os import path
-from os import rename
 from random import randint
 import matplotlib.pyplot as plt
 def DrawPlots(x,y):
@@ -23,16 +22,14 @@ while True:
     total_packets = 0
     timestamp = []
     ids = []
-    packid = -1
-    oldfilename = "OutputFile"+str(randint(0,65536))
-    newfilename = ""
+    last_pack = -1
     clientsocket.settimeout(1)
     try:
         received, address = clientsocket.recvfrom(max_seg_size)
     except timeout:
         continue
     clientsocket.settimeout(3)
-    file = open(oldfilename, "wb")
+    file = None
     start = time()
     startdate = ctime()
     while True:
@@ -41,34 +38,37 @@ while True:
         else:
             total_bytes += len(received)
             total_packets += 1
-            if received[:2] == (packid+1).to_bytes(2, 'big'): 
-                packid += 1
-                print(f"Received packet in order (ID {packid})") 
+            packid = int.from_bytes(received[:2], 'big')
+            fileid = int.from_bytes(received[2:4], 'big')
+            if packid == last_pack+1: 
+                last_pack += 1
+                print(f"Received packet in order (Packet ID {packid}) (File ID {fileid})") 
                 if packid == 0:
-                    newfilename = received[4:-2].decode()
+                    filename = received[4:-4].decode()
+                    if path.exists(filename):
+                        c = 2
+                        while path.exists(filename+" "+str(c)):
+                            c += 1
+                        filename += " "+str(c)
+                    file = open(filename, "wb")
                 else: 
-                    file.write(received[4:-2])
+                    file.write(received[4:-4])
                 ids.append(packid)
                 timestamp.append(time())
-                if received[-2:] == b'\xff\xff':
-                    print("Receiver UDP File Transfer has successfully finished!")
-                    clientsocket.sendto((packid).to_bytes(2,'big')+b'\x00\x00',address) # reached or not, i will finish!
+                if received[-4:] == b'\xff\xff\xff\xff':
+                    print(f"Receiver UDP File Transfer (File ID {fileid}) has successfully finished!")
+                    clientsocket.sendto(received[:4],address) # reached or not, i will finish!
+                    file.close()
                     break
-            else: print(f"Received packet out of order (ID {int.from_bytes(received[:2],'big')})")
-        if packid>=0: clientsocket.sendto((packid).to_bytes(2,'big')+b'\x00\x00',address)
+            else: print(f"Received packet out of order (Packet ID {packid}) (File ID {fileid})")
+        if last_pack>=0: clientsocket.sendto((last_pack).to_bytes(2,'big')+received[2:4],address)
         try: received, address = clientsocket.recvfrom(max_seg_size)
         except timeout:
             print("Server connection is lost (timeout)")
+            file.close()
             break
     end = time()
     enddate = ctime()
-    file.close()
-    if path.exists(newfilename):
-        c = 2
-        while path.exists(newfilename+" ("+str(c)+")"):
-            c += 1
-        newfilename += " ("+str(c)+")"
-    rename(oldfilename,newfilename)
     print(f"Start time: {startdate}")
     print(f"End time: {enddate}")
     print(f"Time elapsed: {end-start:.2f} seconds")
